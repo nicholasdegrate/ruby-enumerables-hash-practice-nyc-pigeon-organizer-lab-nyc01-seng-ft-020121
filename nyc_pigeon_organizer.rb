41556d9e4ca4f4ee37d4d0fa638e2f95afcb180e
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-28 21:50:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = data.each_with_object({}) do |(k, v), list|
+    v.each do |inner_k, names|
+      names.each do |name|
+        if !list[name]
+          list[name] = {}
+        end
+        if !list[name][k]
+          list[name][k] = []
+        end
+        list[name][k].push(inner_k.to_s)
+      end 
+    end 
+  end 
+  list
 end
